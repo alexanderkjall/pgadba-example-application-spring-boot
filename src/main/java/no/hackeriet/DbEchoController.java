58c9d514174e878fc760b2453f2674f682befd8e
Alexander Kjäll <alexander.kjall@gmail.com>
2018-11-12 19:26:07 +0100

    source code for the sync performance test

--- src/main/java/no/hackeriet/DbEchoController.java
+++ src/main/java/no/hackeriet/DbEchoController.java
@@ -1,43 +1,42 @@
 package no.hackeriet;
 
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import jdk.incubator.sql2.AdbaType;
-import jdk.incubator.sql2.DataSource;
-import jdk.incubator.sql2.DataSourceFactory;
-import jdk.incubator.sql2.Result.RowColumn;
-import jdk.incubator.sql2.Session;
-import jdk.incubator.sql2.Submission;
-import org.springframework.http.MediaType;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DbEchoController {
-    private final DataSource ds;
-    private final Session session;
+    private final Connection connection;
 
     public DbEchoController() {
-        ds = DataSourceFactory.newFactory("org.postgresql.adba.PgDataSourceFactory")
-            .builder()
-            .url("jdbc:postgresql://localhost:5432/test")
-            .username("test")
-            .password("test")
-            .build();
-
-        session = ds.getSession();
+        String url = "jdbc:postgresql://localhost/test";
+        Properties props = new Properties();
+        props.setProperty("user","test");
+        props.setProperty("password","test");
+        props.setProperty("ssl","false");
+        try {
+            connection = DriverManager.getConnection(url, props);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @RequestMapping("/{val}")
-    public CompletableFuture<String> index(@PathVariable("val") String val) {
-        Submission<List<RowColumn>> sub = session.<List<RowColumn>>rowOperation("select $1 as t")
-            .set("$1", val, AdbaType.VARCHAR)
-            .collect(Collectors.toList())
-            .submit();
-
-        return sub.getCompletionStage().thenApply(rc -> rc.get(0).at("t").get(String.class)).toCompletableFuture();
+    public String index(@PathVariable("val") String val) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement("select ? as t");
+        ps.setString(1, val);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()) {
+            return rs.getString("t");
+        } else {
+            throw new RuntimeException("error");
+        }
     }
 }
